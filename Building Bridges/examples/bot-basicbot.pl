2ae586e75f0adf551c0bf44da26f5e97e31bd2c9
Stefan Seifert <nine@detonation.org>
2016-07-12 10:44:21 +0200

    Slightly more readable code

--- Building Bridges/examples/bot-basicbot.pl
+++ Building Bridges/examples/bot-basicbot.pl
@@ -1,15 +1,17 @@
-#!perl5
+#!perl6
 
 use Bot::BasicBot:from<Perl5>;
 
 class P6Bot is Bot::BasicBot {
     method said(%statement) {
-        self.reply($%statement, "Hullo {%statement<who>}!")
-            if %statement<body> eq 'Hi bot!';
-        self.reply($%statement, "I'm the first Perl 6 bot based on Bot::BasicBot!")
-            if %statement<body> eq 'bot who are you?';
-        self.shutdown('leaving...')
-            if %statement<body> eq 'bot quit';
+        given %statement<body> {
+            self.reply: $%statement, "Hullo {%statement<who>}!"
+                when 'Hi bot!';
+            self.reply: $%statement, "I'm the first Perl 6 bot based on Bot::BasicBot!"
+                when 'bot who are you?';
+            self.shutdown: 'leaving...'
+                when 'bot quit';
+        }
     }
 }
 
