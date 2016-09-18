5f289a035817eef88da2940938c3ee3e2d4ccf1e
Stefan Seifert <nine@detonation.org>
2016-09-18 17:23:18 +0200

    Dancr benchmark and comparison against Perl 5

--- Perl 5 and Perl 6 - a great team/examples/Dancr/dancr.pl
+++ Perl 5 and Perl 6 - a great team/examples/Dancr/dancr.pl
@@ -1,10 +1,10 @@
-use v6.c;
-
-use Dancer2:from<Perl5>;
-use DBI:from<Perl5>;
-use Template:from<Perl5>;
+use Dancer2;
+use DBI;
+use File::Spec;
+use File::Slurper qw/ read_text /;
+use Template;
  
-set 'database'     => $*TMPDIR.child('dancr.db');
+set 'database'     => File::Spec->catfile(File::Spec->tmpdir(), 'dancr.db');
 set 'session'      => 'Simple';
 set 'template'     => 'template_toolkit';
 set 'logger'       => 'console';
@@ -18,11 +18,13 @@ set 'layout'       => 'main';
  
 my $flash;
  
-sub set_flash($message) {
+sub set_flash {
+    my $message = shift;
+ 
     $flash = $message;
 }
  
-sub get_flash() {
+sub get_flash {
  
     my $msg = $flash;
     $flash = "";
@@ -30,77 +32,62 @@ sub get_flash() {
     return $msg;
 }
  
-sub connect_db() {
-    my $dbh = DBI.connect("dbi:SQLite:dbname={setting('database')}", Any, Any, ${sqlite_unicode => 1}) or
-        die %*PERL5<$DBI::errstr>;
+sub connect_db {
+    my $dbh = DBI->connect("dbi:SQLite:dbname=".setting('database')) or
+        die $DBI::errstr;
  
     return $dbh;
 }
  
-sub init_db() {
+sub init_db {
     my $db = connect_db();
-    my $schema = slurp 'schema.sql';
-    $db.do($schema) or die $db.errstr;
+    my $schema = read_text('./schema.sql');
+    $db->do($schema) or die $db->errstr;
 }
  
-#hook before_template_render => sub (%tokens) {
-#    %tokens<css_url> = request.base ~ 'css/style.css';
-#    %tokens<login_url> = uri_for('/login');
-#    %tokens<logout_url> = uri_for('/logout');
-#};
-use MONKEY-SEE-NO-EVAL;
-my &hash-filler = EVAL q:to/PERL5/, :lang<Perl5>;
-    sub {
-        my ($source) = @_;
-        return sub {
-            my ($target) = @_;
-            my $data = $source->();
-            $target->{$_} = $data->{$_} foreach keys (%$data);
-        }
-    }
-    PERL5
-hook before_template_render => hash-filler({ ${
-        css_url => request.base ~ 'css/style.css',
-        login_url => uri_for('/login'),
-        logout_url => uri_for('/logout'),
-    } });
+hook before_template_render => sub {
+    my $tokens = shift;
  
-get '/' => {
+    $tokens->{'css_url'} = request->base . 'css/style.css';
+    $tokens->{'login_url'} = uri_for('/login');
+    $tokens->{'logout_url'} = uri_for('/logout');
+};
+ 
+get '/' => sub {
     my $db = connect_db();
     my $sql = 'select id, title, text from entries order by id desc';
-    my $sth = $db.prepare($sql) or die $db.errstr;
-    $sth.execute or die $sth.errstr;
-    template 'show_entries.tt', ${
+    my $sth = $db->prepare($sql) or die $db->errstr;
+    $sth->execute or die $sth->errstr;
+    template 'show_entries.tt', {
         'msg' => get_flash(),
         'add_entry_url' => uri_for('/add'),
-        'entries' => $sth.fetchall_hashref('id'),
+        'entries' => $sth->fetchall_hashref('id'),
     };
 };
  
-post '/add' => {
-    unless session('logged_in') {
+post '/add' => sub {
+    if ( not session('logged_in') ) {
         send_error("Not logged in", 401);
     }
  
     my $db = connect_db();
     my $sql = 'insert into entries (title, text) values (?, ?)';
-    my $sth = $db.prepare($sql) or die $db.errstr;
-    $sth.execute(body_parameters.get('title'), body_parameters.get('text'))
-        or die $sth.errstr;
+    my $sth = $db->prepare($sql) or die $db->errstr;
+    $sth->execute(params->{'title'}, params->{'text'}) or die $sth->errstr;
  
     set_flash('New entry posted!');
     redirect '/';
 };
  
-any $['get', 'post'], '/login', sub (*@a) {
+any ['get', 'post'] => '/login' => sub {
     my $err;
  
-    if ( request.method eq "POST" ) {
+    if ( request->method() eq "POST" ) {
         # process form input
-        if body_parameters.get('username') ne setting('username') {
+        if ( params->{'username'} ne setting('username') ) {
             $err = "Invalid username";
         }
-        elsif body_parameters.get('password') ne setting('password') {
+        elsif ( params->{'password'} ne setting('password') ) {
             $err = "Invalid password";
         }
         else {
@@ -111,19 +98,17 @@ any $['get', 'post'], '/login', sub (*@a) {
    }
  
    # display login form
-   template 'login.tt', ${
+   template 'login.tt', {
        'err' => $err,
    };
  
 };
  
-get '/logout' => {
-   app.destroy_session;
+get '/logout' => sub {
+   app->destroy_session;
    set_flash('You are logged out.');
    redirect '/';
 };
  
 init_db();
 start;
-
-# vim: ft=perl6
