43985d1489c5c12a08b5e1a42db0460defe7a16a
Stefan Seifert <nine@detonation.org>
2017-02-03 10:01:14 +0100

    Highlight psgi files

--- Web development and Perl 6/examples/highlight.sh
+++ Web development and Perl 6/examples/highlight.sh
@@ -1,2 +1,5 @@
 CSSPATH=$(dirname $0)
-for F in *.pl6; do vim -c TOhtml -c wqa $F && mv $F.html $F.html.orig && head -n10 $F.html.orig > $F.html && echo '<link rel="stylesheet" type="text/css" href="'$CSSPATH'/syntax.css">' >> $F.html && tail -n+30 $F.html.orig >> $F.html && rm $F.html.orig ; done
+for F in *.pl6 *.pm *.pm6 *.p6sgi ; do vim -c TOhtml -c wqa $F && mv $F.html $F.html.orig && head -n10 $F.html.orig > $F.html && echo '<link rel="stylesheet" type="text/css" href="'$CSSPATH'/syntax.css">' >> $F.html && tail -n+30 $F.html.orig >> $F.html && rm $F.html.orig ; done
+rm -f \*.pl6.html
+rm -f \*.pm.html
+rm -f \*.pm6.html
